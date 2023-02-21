69fee73d2e774140188de569e5ab200d4aa7eb86
n2posa00 <n2posa00@students.oamk.fi>
2023-02-21 11:06:49 +0200

    Ensimmmäinen toimiva versio

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+header("Access-Control-Allow-Origins:*");
+header('Content-Type: application/json');
+$data = array(
+    array('firstname' => 'Samuli', 'lastname' => 'Pohjola', 'email' => 's.pohjola@oamk.fi'),
+    array('firstname' => 'Timo', 'lastname' => 'Kuusinen', 'email' => 't.kuusinen@oamk.fi'),
+    array('firstname' => 'Seppo', 'lastname' => 'Ismo', 'email' => 's.ismo@oamk.fi'),
+    array('firstname' => 'Mikael', 'lastname' => 'Nilkkilä', 'email' => 'm.nilkkila@oamk.fi'),
+    array('firstname' => 'Ossi', 'lastname' => 'Osteri', 'email' => 'o.osteri@oamk.fi'),
+);
+$json = json_encode($data, JSON_PRETTY_PRINT);
+print $json;
